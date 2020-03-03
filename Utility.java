012de01a382a3a00e783cea91a89cf1fde4a5439
ainajain1 <47808336+ainajain1@users.noreply.github.com>
2020-03-03 22:10:54 +0530

    Delete Utility.java

--- Utility.java
+++
@@ -1,8 +0,0 @@
-public class Utility {
-
-    public static int NUMBER_OF_CARDS_PER_SUIT = 13;
-
-    public static String getPlayerNameFromNumber(int number) {
-        return "Player " + number;
-    }
-}
